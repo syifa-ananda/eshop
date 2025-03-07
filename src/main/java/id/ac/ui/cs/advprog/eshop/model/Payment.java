8e329cf8b0631d78318f0f80d3f0acbcd00262cd
syifa-ananda <syifaananda36@gmail.com>
2025-03-07 12:45:23 +0700

    [GREEN] Implement Payment mdoel

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import lombok.Getter;
 
 import java.util.Map;
@@ -12,14 +13,21 @@ public class Payment {
     Map<String, String> paymentData;
 
     public Payment(String id, String method, Map<String,String> paymentData) {
-
+        this.id = id;
+        this.method = method;
+        this.paymentData = paymentData;
     }
 
     public Payment(String id, String method, Map<String,String> paymentData, String status) {
-
+        this(id,method, paymentData);
+        this.setStatus(status);
     }
 
     public void setStatus(String status) {
-
+        if (PaymentStatus.contains(status)) {
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 }
