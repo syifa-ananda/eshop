d8cbb5f4476a339fbeb247949ce8344f7e85954c
syifa-ananda <syifaananda36@gmail.com>
2025-03-07 13:36:13 +0700

    [GREEN] Implement PaymentRepository class

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -0,0 +1,25 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PaymentRepository {
+
+    private Map<String, Payment> paymentStore = new HashMap<>();
+
+    public Payment save(Payment payment) {
+        paymentStore.put(payment.getId(), payment);
+        return payment;
+    }
+
+    public Payment getPayment(String id) {
+        return paymentStore.get(id);
+    }
+
+    public List<Payment> getAllPayments() {
+        return new ArrayList<>(paymentStore.values());
+    }
+}
